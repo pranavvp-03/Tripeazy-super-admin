574d9196cb42399a7db3eeed742a52ffa1e8e343
pranav vp <vppranav03@gmail.com>
2024-12-30 14:43:13 +0530

    backend admin creation

--- server/model/Admin.js
+++ server/model/Admin.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
+const Role = require("./role");  // Import the Role model
 
-// Admin Schema
 const adminSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
     role: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Role', // References the Role model 
-        required: true, 
-        default: 'superAdmin' // Default role for the first user
+        type: mongoose.Schema.Types.ObjectId,  // Use ObjectId to reference Role model
+        ref: "Role", 
+        required: true 
     }
 });
+
 
 module.exports = mongoose.model("Admin", adminSchema);

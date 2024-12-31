f44492ba30228b433a3a6ef2d177d4bdd3e9357c
pranav vp <vppranav03@gmail.com>
2024-12-31 11:16:06 +0530

    pull request

--- server/model/Admin.js
+++ server/model/Admin.js
@@ -6,6 +6,7 @@ const adminSchema = new mongoose.Schema({
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
     role: { 
+
         type: mongoose.Schema.Types.ObjectId, // Reference Role model
         ref: "Role", 
         required: true 

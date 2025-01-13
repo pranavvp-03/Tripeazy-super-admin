0c9201f5ff92b0d782473cd93a53f1b3461b7010
famnas321 <famnaspk380@gmail.com>
2025-01-13 21:45:05 +0530

    committed on 13.1.2025

--- client/src/pages/createAdminTab/CreateNewAdmin.jsx
+++ client/src/pages/createAdminTab/CreateNewAdmin.jsx
@@ -12,9 +12,13 @@ function CreateNewAdmin() {
   }
   const handleImage=(e)=>{
     const file=e.target.files[0]
-    console.log(file)
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setImage(imageUrl); // Update the state with the new image URL
+    }
+    // console.log(file)
     console.log("Is it a File object?", file instanceof File);
-     setImage(e.target.files)
+    //  setImage(e.target.files)
   }
   
   return (
@@ -180,32 +184,12 @@ function CreateNewAdmin() {
     <span className="inline-block w-[62px] h-[62px] bg-gray-100 rounded-full overflow-hidden" onClick={handleClick}>
        {image ? <img src={image}
              
-              className="w-full h-full object-cover"
-       alt="" /> : <svg
-        className="w-full h-full text-gray-300"
-        width="16"
-        height="16"
-        viewBox="0 0 16 16"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <rect
-          x="0.62854"
-          y="0.359985"
-          width="15"
-          height="15"
-          rx="7.5"
-          fill="white"
-        ></rect>
-        <path
-          d="M8.12421 7.20374C9.21151 7.20374 10.093 6.32229 10.093 5.23499C10.093 4.14767 9.21151 3.26624 8.12421 3.26624C7.0369 3.26624 6.15546 4.14767 6.15546 5.23499C6.15546 6.32229 7.0369 7.20374 8.12421 7.20374Z"
-          fill="currentColor"
-        ></path>
-        <path
-          d="M11.818 10.5975C10.2992 12.6412 7.42106 13.0631 5.37731 11.5537C5.01171 11.2818 4.69296 10.9631 4.42107 10.5975C4.28982 10.4006 4.27107 10.1475 4.37419 9.94123L4.51482 9.65059C4.84296 8.95684 5.53671 8.51624 6.30546 8.51624H9.95231C10.7023 8.51624 11.3867 8.94749 11.7242 9.62249L11.8742 9.93184C11.968 10.1475 11.9586 10.4006 11.818 10.5975Z"
-          fill="currentColor"
-        ></path>
-      </svg>}
+             className="w-24 h-24 rounded-full object-cover border-2 border-gray-300"
+             alt="" /> : <img
+             src="Design Studio.jpg"
+             className="w-24 h-24 rounded-full object-cover border-2 border-gray-300"
+             alt="Profile"
+           />}
     </span>
     <p>Add Profile</p>
     <input type="file" 

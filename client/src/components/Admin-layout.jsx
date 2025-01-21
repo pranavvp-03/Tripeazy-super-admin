419dc5c0b3e93e9c381a3082876af39875f1dc26
famnas321 <famnaspk380@gmail.com>
2025-01-22 00:25:11 +0530

    committed on 22.1.2025

--- client/src/components/Admin-layout.jsx
+++ client/src/components/Admin-layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
 import AdminNavbar from "./Admin-navbar";
 
 const AdminDashboardLayout = () => {
   return (
     <div className="flex h-screen">
-      <AdminNavbar />
+      {/* <AdminNavbar /> */}
+      <Navbar/>
         <div className="flex-1 p-1 overflow-auto bg-lightBg">
       <Outlet />
       </div>

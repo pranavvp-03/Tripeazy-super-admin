7adf5fdf70cca610eb9e9525fc33acabc22ef38e
famnas321 <famnaspk380@gmail.com>
2025-02-12 13:21:06 +0530

    committed on 13,2,2025

--- client/src/components/CommonNav.jsx
+++ client/src/components/CommonNav.jsx
@@ -4,9 +4,9 @@ import ProfileDropdown from './ProfileMenu';
 function CommonNav() {
   return (
     <div>
-      <nav className="bg-gray-900 border-gray-200 -mr-2 -mt-1 rounded-t-md flex flex-col items-center h-28">
+      <nav className="bg-gray-900 border-gray-200 -mr-2 -mt-1  flex flex-col items-center h-28">
         
-        <div className="flex items-center justify-center w-full">
+        <div className="flex items-center justify-center w-full mt-2">
           <h1 className="text-white text-4xl">Home</h1>
         </div>
         
@@ -15,16 +15,16 @@ function CommonNav() {
             <a href="https://flowbite.com/">
               <img
                 src="https://flowbite.com/docs/images/logo.svg"
-                className="h-8"
+                className="h-8 mb-3"
                 alt="Flowbite Logo"
               />
             </a>
-            <span className="self-center text-2xl font-semibold whitespace-nowrap text-white dark:text-white">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap text-white dark:text-white mb-4">
               Trippeazy
             </span>
           </div>
           
-          <div className="align mt-2">
+          <div className="align mr-5 -mt-4">
             <ProfileDropdown />
           </div>
         </div>

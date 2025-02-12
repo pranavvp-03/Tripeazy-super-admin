5a5a179f025efb5af6021ab50ebd7a853928ab5a
famnas321 <famnaspk380@gmail.com>
2025-02-12 20:26:38 +0530

    committed on 12.2.2025 /3

--- client/src/pages/createAdminTab/ViewProfile.jsx
+++ client/src/pages/createAdminTab/ViewProfile.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useState } from "react";
 
-function ViewProfile() {
+const ViewProfile = () => {
+  const [username, setUsername] = useState("");
+
   return (
-    <div>ViewProfile</div>
-  )
-}
+    <div>
+   <div className="relative my-4 border-b-2 focus-within:border-blue-500 w-6/12 mx-auto">
+  <input
+    type="text"
+    name="username"
+    id="username"
+    value=""
+    onChange={(e) => setUsername(e.target.value)}
+    placeholder=" "
+    className="block w-full appearance-none focus:outline-none bg-transparent peer"
+  />
+  <label
+    htmlFor="username"
+    className="absolute left-0 top-1/2 -translate-y-1/2 text-gray-500 transition-all duration-200 ease-in-out"
+  >
+    Username
+  </label>
+</div>
 
-export default ViewProfile 
+
+    </div>
+  );
+};
+
+export default ViewProfile;
